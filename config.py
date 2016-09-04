7f4478038480924509daef029f1851feb6a6bc2b
Mark Neumann <ucabmrn@ucl.ac.uk>
2016-09-04 11:31:43 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,17 +6,14 @@ from __future__ import print_function
 class DAConfig(object):
 
   init_scale = 0.01
-  learning_rate = 0.001
+  learning_rate = 0.05
   max_grad_norm = 5
-  num_layers = 2
-  num_steps = 20
   hidden_size = 200
-  max_epoch = 4
   max_max_epoch = 16
   keep_prob = 0.8
   lr_decay = 0.8
   batch_size = 32
-  vocab_size = 10000
+  vocab_size = 10000 # overriden by the actual vocab size you generate
 
   embedding_size = 300
   embedding_reg = None
